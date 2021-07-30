0eae09de7ab15448051aaebc4aa51d5a63afa333
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2021-07-31 08:48:16 +0900

    Revert "chore: install contentful and define api client"
    This reverts commit 0c55d255d94eba8d322f3ac662db47820c03963f.

--- contentful.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from 'contentful'
-
-const client = createClient({
-  accessToken: process.env.CONTENTFUL_DELIVERY_TOKEN || '',
-  space: process.env.CONTENTFUL_SPACE_ID || '',
-});
-
-export default client;
