be96279d30bea6a3a2adfe69bc7d98045466a278
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2021-07-31 09:54:46 +0900

    style: temp width update

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,40 +21,18 @@ export default function Home({ blog }) {
           I write a blog to record tasting comments and some learnings about wine!
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>{blog[0].title}</h2>
-            <p>Created at</p>
-            <p>生産者</p>
-            <p>AOC</p>
-            <p>Vintage</p>
-          </a>
-
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Wine Name</h2>
-            <p>Created at</p>
-            <p>生産者</p>
-            <p>AOC</p>
-            <p>Vintage</p>
-          </a>
-
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Wine Name</h2>
-            <p>Created at</p>
-            <p>生産者</p>
-            <p>AOC</p>
-            <p>Vintage</p>
-          </a>
-
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Wine Name</h2>
-            <p>Created at</p>
-            <p>生産者</p>
-            <p>AOC</p>
-            <p>Vintage</p>
-          </a>
-        </div>
-      </main>
+        {blog.map((post) => (
+          <div className={styles.grid} key={post.id}>
+            <a href="https://nextjs.org/docs" className={styles.card}>
+              <h2>{post.title}</h2>
+              <p>Created at</p>
+              <p>生産者</p>
+              <p>AOC</p>
+              <p>Vintage</p>
+            </a>
+          </div>
+        ))}
+        </main>
 
       <footer className={styles.footer}>
         {/* ? Link to Twitter, Github? */}
