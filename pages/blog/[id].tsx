962eb23eb2e7ab4363cba011e22f833711ba9aa0
Takuya Kikuchi <takuyakikuch@gmail.com>
2021-08-07 16:47:36 +0900

    fix: preview goes to 404

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,9 +1,14 @@
+import DefaultErrorPage from 'next/error'
 import { client } from "../../libs/client";
 
 import styles from "../../styles/Blog.module.css";
 
 // Todo: update type
 export default function BlogId({ blog }: any) {
+  if (!blog) {
+    <DefaultErrorPage statusCode={404} />
+  }
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
@@ -23,7 +28,7 @@ export const getStaticPaths = async () => {
   const data: any = await client.get({ endpoint: "blog" });
 
   const paths = data.contents.map((content: any) => `/blog/${content.id}`);
-  return { paths, fallback: false };
+  return { paths, fallback: true };
 };
 
 export const getStaticProps = async (context: any) => {
