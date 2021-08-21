99516936dcdf3df7e521f87af9a9beba004eb0e6
Takuya Kikuchi <takuyakikuch@gmail.com>
2021-08-21 17:07:37 +0900

    refactor: format existing files

--- libs/client.ts
+++ libs/client.ts
@@ -1,7 +1,5 @@
 import { createClient } from 'microcms-js-sdk';
 
-
-
 export const client = createClient({
   serviceDomain: 'wine-blog',
   apiKey: process.env.MICRO_CMS_API_KEY || '',
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import dayjs from 'dayjs'
-import { client } from "../libs/client";
+import dayjs from 'dayjs';
+import { client } from '../libs/client';
 
-import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
-import styles from "../styles/Home.module.css";
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
 
 // Todo: update type
 export default function Home({ blog }: any) {
@@ -12,8 +12,8 @@ export default function Home({ blog }: any) {
     <div className={styles.container}>
       <Head>
         <title>My Wine Blog</title>
-        <meta name="description" content="My wine blog" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name='description' content='My wine blog' />
+        <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main className={styles.main}>
@@ -24,33 +24,29 @@ export default function Home({ blog }: any) {
         </p>
 
         <div className={styles.grid}>
-
-        {/* Todo: update type */}
-        {blog.map((post: any) => (
-          <Link href={`/blog/${post.id}`} key={post.id}>
-            <a className={styles.card}>
-              <h2>{post.title}</h2>
-              <p>生産者: {post.producer}</p>
-              <p>生産国: {post.country}</p>
-              <p>原産地呼称: {post.aoc}</p>
-              <p>ヴィンテージ: {post.vintage}</p>
-              <p>ぶどう品種: {post.grapes}</p>
-              <p>投稿日: {dayjs(post.publishedAt).format('YYYY-MM-DD')}</p> 
-            </a>
-          </Link>
-        ))}
+          {/* Todo: update type */}
+          {blog.map((post: any) => (
+            <Link href={`/blog/${post.id}`} key={post.id}>
+              <a className={styles.card}>
+                <h2>{post.title}</h2>
+                <p>生産者: {post.producer}</p>
+                <p>生産国: {post.country}</p>
+                <p>原産地呼称: {post.aoc}</p>
+                <p>ヴィンテージ: {post.vintage}</p>
+                <p>ぶどう品種: {post.grapes}</p>
+                <p>投稿日: {dayjs(post.publishedAt).format('YYYY-MM-DD')}</p>
+              </a>
+            </Link>
+          ))}
         </div>
-        </main>
+      </main>
 
       <footer className={styles.footer}>
         <span>
           Maintained by
-          <a 
-            href="https://twitter.com/_takuyakikuchi"
-            target="_blank"
-            rel="noopener noreferrer">
+          <a href='https://twitter.com/_takuyakikuchi' target='_blank' rel='noopener noreferrer'>
             <span className={styles.twitterAccount}>@_takuyakikuchi</span>
-            <Image src="/twitter.svg" alt="Twitter Logo" width={16} height={16} />
+            <Image src='/twitter.svg' alt='Twitter Logo' width={16} height={16} />
           </a>
         </span>
       </footer>
@@ -60,7 +56,7 @@ export default function Home({ blog }: any) {
 
 export const getStaticProps = async () => {
   // Todo: type should be changed
-  const data: any = await client.get({ endpoint: "blog" });
+  const data: any = await client.get({ endpoint: 'blog' });
 
   return {
     props: {
