97adeb36af5825b5453191f7f38506d9d1e2b4ab
Takuya Kikuchi <takuyakikuch@gmail.com>
2022-01-18 17:39:46 +0900

    feat: replace global scss to styled-component createGlobalStyle

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,15 @@
-// https://styled-components.com/docs/advanced#nextjs
-// https://github.com/vercel/next.js/blob/main/examples/with-styled-components/pages/_document.js
-import Document, { DocumentContext } from 'next/document';
+/**
+ * Adding styled-components in Next.js
+ * https://styled-components.com/docs/advanced#nextjs
+ * https://github.com/vercel/next.js/blob/main/examples/with-styled-components/pages/_document.js
+ */
+
+/**
+ * Adding google fonts in Next.js
+ * https://github.com/vercel/next.js/blob/main/examples/with-styled-components/pages/_document.js
+ */
+
+import Document, { DocumentContext, Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
@@ -28,4 +37,25 @@ export default class MyDocument extends Document {
       sheet.seal();
     }
   }
+
+  render() {
+    return (
+      <Html lang='ja'>
+        <Head>
+          <link
+            href='https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@400;700&family=Noto+Sans+JP:wght@400;700&display=swap'
+            rel='stylesheet'
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+/**
+ *
+ */
