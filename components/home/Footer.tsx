32435a0d9f5f0e98ab00c80896148cef795cc1d3
Takuya Kikuchi <takuyakikuch@gmail.com>
2022-02-13 12:59:40 +0900

    [settings] GA: add GA notification in the footer

--- components/home/Footer.tsx
+++ components/home/Footer.tsx
@@ -14,42 +14,57 @@ export default function Footer() {
 
   return (
     <Wrapper>
-      <Twitter>
-        <span>Maintained by</span>
+      <Copyright>©︎takuyakikuchi 2022</Copyright>
+      <ContactAt
+        href='https://twitter.com/_takuyakikuchi'
+        target='_blank'
+        rel='noopener noreferrer'
+        onClick={handleTwitterClick}
+      >
+        <span>Contact @</span>
+        <Image src='/twitter.svg' alt='Twitter Logo' width={16} height={16} />
+      </ContactAt>
+      <p>
+        このサイトはGoogle Analyticsを使用しています。
         <a
-          href='https://twitter.com/_takuyakikuchi'
+          href='https://policies.google.com/technologies/partner-sites?hl=ja'
           target='_blank'
           rel='noopener noreferrer'
-          onClick={handleTwitterClick}
         >
-          <span>@_takuyakikuchi</span>
-          <Image src='/twitter.svg' alt='Twitter Logo' width={16} height={16} />
+          詳しく見る
         </a>
-      </Twitter>
+      </p>
     </Wrapper>
   );
 }
 
 export const Wrapper = styled.footer`
   display: flex;
-  align-items: center;
+  flex-direction: column;
   justify-content: center;
-  width: 100%;
-  height: 100px;
-  border-top: 1px solid ${({ theme }) => theme.colors.lightGray};
-`;
-
-export const Twitter = styled.div`
-  display: flex;
+  align-items: center;
   gap: 4px;
-  align-items: center;
-  justify-content: center;
+  width: 100%;
+  padding: 24px;
+  border-top: 1px solid ${({ theme }) => theme.colors.lightGray};
+  font-size: 0.9rem;
 
   a {
-    display: inline-flex;
-    gap: 4px;
-    align-items: center;
-    color: inherit;
+    color: ${({ theme }) => theme.colors.primary};
     text-decoration: none;
+
+    &:hover {
+      text-decoration: revert;
+    }
   }
+`;
+
+export const Copyright = styled.span`
+  display: inline-block;
+`;
+
+export const ContactAt = styled.a`
+  display: inline-flex;
+  gap: 4px;
+  align-items: center;
 `;
